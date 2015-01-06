5959c58292e603e317f53bedcb7631bfbe564e19
Thorsten Ploß <1ploss@informatik.uni-hamburg.de>
2015-01-06 12:55:42 +0100

    etwas rumgebastel

---
+++ GUI.py
@@ -0,0 +1,5 @@
+import tkinter
+
+GUI = tkinter.Tk()
+
+tkinter.mainloop()
---
+++ main.py
@@ -0,0 +1,8 @@
+import GUI
+#import pictureloader as picload
+
+def main():
+    __gui = GUI()
+
+if __name__ == '__main__':
+    main()
